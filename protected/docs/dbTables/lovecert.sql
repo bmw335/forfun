76196ca18d1313119eb28618b9fe6f1d6b28167b
BMW335 <164140227@qq.com>
2014-02-13 22:55:23 +0800

    refs #4 merge {admin, user} table to {user} table only
    update sql file

--- protected/docs/dbTables/lovecert.sql
+++ protected/docs/dbTables/lovecert.sql
@@ -10,27 +10,10 @@ Target Server Type    : MYSQL
 Target Server Version : 50611
 File Encoding         : 65001
 
-Date: 2014-02-11 21:29:39
+Date: 2014-02-13 22:54:33
 */
 
 SET FOREIGN_KEY_CHECKS=0;
-
--- ----------------------------
--- Table structure for lc_admin
--- ----------------------------
-DROP TABLE IF EXISTS `lc_admin`;
-CREATE TABLE `lc_admin` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_name` varchar(64) NOT NULL,
-  `password` varchar(64) NOT NULL,
-  `email` varchar(320) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of lc_admin
--- ----------------------------
-INSERT INTO `lc_admin` VALUES ('1', 'admin', 'admin', '');
 
 -- ----------------------------
 -- Table structure for lc_certificate
@@ -88,18 +71,22 @@ CREATE TABLE `lc_password` (
 DROP TABLE IF EXISTS `lc_user`;
 CREATE TABLE `lc_user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `qq_uid` varchar(320) NOT NULL,
-  `real_name` varchar(64) DEFAULT NULL,
   `nick_name` varchar(128) NOT NULL,
+  `role` tinyint(4) DEFAULT '0',
+  `qq_uid` varchar(320) DEFAULT NULL,
+  `user_name` varchar(64) DEFAULT NULL,
+  `password` varchar(64) DEFAULT NULL,
+  `real_name` varchar(64) DEFAULT NULL,
   `is_male` bit(1) DEFAULT NULL,
   `email` varchar(320) DEFAULT NULL,
   `birthday` date DEFAULT NULL,
   `register_time` datetime DEFAULT NULL,
-  `point` int(11) NOT NULL DEFAULT '0',
+  `point` int(11) DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of lc_user
 -- ----------------------------
-INSERT INTO `lc_user` VALUES ('1', '164140227', '方梁康', 'flk', '', 'fangliangkang@gmail.com', '1986-09-25', '2014-02-09 00:00:00', '100');
+INSERT INTO `lc_user` VALUES ('2', 'ImAdmin', '1', null, 'admin', 'admin', null, null, null, null, null, '0');
+INSERT INTO `lc_user` VALUES ('6', 'ImNormalUser', '0', 'qqOpenID_1234', null, null, null, null, null, null, null, '10');
