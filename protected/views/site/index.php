c99191df1f73032929362322d2081aafa604ed1a
6f <6Feng@6Feng-PC>
2014-02-19 23:10:28 +0800

    refs #liu add qq login

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -5,6 +5,11 @@ $this->pageTitle=Yii::app()->name;
 ?>
 
 <div class="input-group input-group-lg" id="search-bar">
-  <input type="text" class="form-control">
-  <span id="search-button" class="input-group-addon ">Go</span>
+  <input type="text" class="form-control" placeholder="他/她常用的名字或昵称">
+  <span id="search-button" class="input-group-addon "></span>
+</div>
+
+
+<div id="qq-login">
+       
 </div>
